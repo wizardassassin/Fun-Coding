4b48ad072ae00125401c8a9e8f34db6554e784c9
wizard_assassin <victorzhai0916@gmail.com>
2021-01-20 14:50:00 -0800

    Some Formatting and Prep

--- Project Euler/index.js
+++ Project Euler/index.js
@@ -1,3 +1,12 @@
-import problem1 from './solutions/problem10.js';
+// import * as fs from 'fs';
 
-console.log(problem1())
+// const solutions = fs.readdirSync('./solutions');
+
+// console.log(solutions)
+
+// import * as problem from './solutions/index.js';
+
+// console.log(problem.problem1())
+
+import debug from './solutions/problem011.js';
+console.log(debug())
