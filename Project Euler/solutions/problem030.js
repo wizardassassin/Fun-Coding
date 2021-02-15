4cd8ad2aa41486a832f4bd7ee6daaa301b759648
wizard_assassin <60045243+wizardassassin@users.noreply.github.com>
2021-02-14 20:16:04 -0800

    Problems 29 to 37

--- Project Euler/solutions/problem030.js
+++ Project Euler/solutions/problem030.js
@@ -1,7 +1,16 @@
 /*
-// Insert Title Here
+Digit fifth powers
 
-// Insert Problem Here
+Surprisingly there are only three numbers that can be written as the sum of fourth powers of their digits:
+
+1634 = 1^4 + 6^4 + 3^4 + 4^4
+8208 = 8^4 + 2^4 + 0^4 + 8^4
+9474 = 9^4 + 4^4 + 7^4 + 4^4
+As 1 = 1^4 is not a sum it is not included.
+
+The sum of these numbers is 1634 + 8208 + 9474 = 19316.
+
+Find the sum of all the numbers that can be written as the sum of fifth powers of their digits.
 
 https://projecteuler.net/problem=30
 
@@ -10,6 +19,16 @@ Attribution-NonCommercial-ShareAlike 4.0 International (CC BY-NC-SA 4.0).
 This does not apply to the solution/code.
 */
 
-export default function problem30(n = -1) {
-    
+export default function problem30(n = 5) {
+    let acc = 0,
+        en = 1,
+        a = 9 ** n;
+    while (10 ** en < en * a)
+        en++;
+    en = 10 ** en;
+    for (let i = 10; i <= en; i++) {
+        if (String(i).split('').reduce((acc, cur) => acc + cur ** n, 0) == i)
+            acc += i;
+    }
+    return acc;
 }
--- Project Euler/solutions/problem036.js
+++ Project Euler/solutions/problem036.js
@@ -1,7 +1,11 @@
 /*
-// Insert Title Here
+Double-base palindromes
 
-// Insert Problem Here
+The decimal number, 585 = 1001001001^^2 (binary), is palindromic in both bases.
+
+Find the sum of all numbers, less than one million, which are palindromic in base 10 and base 2.
+
+(Please note that the palindromic number, in either base, may not include leading zeros.)
 
 https://projecteuler.net/problem=36
 
@@ -10,6 +14,21 @@ Attribution-NonCommercial-ShareAlike 4.0 International (CC BY-NC-SA 4.0).
 This does not apply to the solution/code.
 */
 
-export default function problem36(n = -1) {
-    
+export default function problem36(n = 1000000) {
+    let acc = 0;
+    for (let i = 0; i < n; i++) {
+        if (palindrome(i.toString()) && palindrome(i.toString(2))) {
+            acc += i;
+        }
+
+    }
+    return acc;
+}
+
+function palindrome(a) {
+    for (let s = 0, e = a.length - 1; s <= e; s++, e--) {
+        if (a[s] != a[e])
+            return false;
+    }
+    return true;
 }
