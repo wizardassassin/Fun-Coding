42680360972e209b559d0fcd15004de9d2a5bda4
wizard_assassin <60045243+wizardassassin@users.noreply.github.com>
2021-02-17 15:00:49 -0800

    Problems 38 to 40; debugging + answers file; neater "run all" file; Subscript formatting;

--- Project Euler/solutions/problem025.js
+++ Project Euler/solutions/problem025.js
@@ -3,24 +3,24 @@
 
 The Fibonacci sequence is defined by the recurrence relation:
 
-F^^n = F^^n−1 + F^^n−2, where F^^1 = 1 and F^^2 = 1.
+F_n = F_n−1 + F_n−2, where F_1 = 1 and F_2 = 1.
 
 Hence the first 12 terms will be:
 
-F^^1 = 1
-F^^2 = 1
-F^^3 = 2
-F^^4 = 3
-F^^5 = 5
-F^^6 = 8
-F^^7 = 13
-F^^8 = 21
-F^^9 = 34
-F^^10 = 55
-F^^11 = 89
-F^^12 = 144
+F_1 = 1
+F_2 = 1
+F_3 = 2
+F_4 = 3
+F_5 = 5
+F_6 = 8
+F_7 = 13
+F_8 = 21
+F_9 = 34
+F_10 = 55
+F_11 = 89
+F_12 = 144
 
-The 12th term, F^^12, is the first term to contain three digits.
+The 12th term, F_12, is the first term to contain three digits.
 
 What is the index of the first term in the Fibonacci sequence to contain 1000 digits?
 
