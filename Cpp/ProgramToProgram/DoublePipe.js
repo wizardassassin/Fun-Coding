32e44adb30a455bb0cdb4a04d3b6d4c8da91bd34
wizard_assassin <victorzhai0916@gmail.com>
2022-06-27 19:11:52 -0700

    Coin Flip Simulation

--- Cpp/ProgramToProgram/DoublePipe.js
+++ Cpp/ProgramToProgram/DoublePipe.js
@@ -2,60 +2,129 @@
 
 import { spawn, exec } from "child_process";
 import fs from "fs";
+import { performance } from "perf_hooks";
 import { promisify } from "util";
 
 const { Path1, Path2 } = JSON.parse(
     fs.readFileSync("./.hide.path.json", "utf-8")
 );
 
+const runBuild = true;
+const runTimes = 10;
+
+const logOutput = true;
+const logError = true;
+const logExit = false;
+
+const args = "-Og";
+const name = "coin_flip_cheater";
+
+console.log({ args, name });
+
+const Path1Parsed = Path1.replaceAll("${args}", args).replaceAll(
+    "${name}",
+    name
+);
+const Path2Parsed = Path2.replaceAll("${args}", args).replaceAll(
+    "${name}",
+    name
+);
+
 const promiseExec = promisify(exec);
+const promiseSpawn = promisify(spawn);
 
 const shellArgs = {
     shell: "pwsh",
     cwd: "../build",
 };
+const shellArgs2 = {
+    shell: "pwsh",
+    cwd: "./",
+};
+
+if (runBuild) {
+    await build();
+}
+
+execute();
+
+// for (let i = 0; i < runTimes; i++) {
+//     // execute();
+//     await execute();
+// }
 
 // Build Programs
-const start = Date.now();
-const buildProgram = promiseExec(Path1, shellArgs);
-const buildUser = promiseExec(Path2, shellArgs);
-await Promise.all([buildProgram, buildUser]); // Error Handling?
-const start2 = Date.now();
-console.log("Build Time:", start2 - start);
+async function build() {
+    const start = Date.now();
+    const buildProgram = promiseExec(Path1Parsed, shellArgs2);
+    const buildUser = promiseExec(Path2Parsed, shellArgs2);
+    await Promise.all([buildProgram, buildUser]); // Error Handling?
+    console.log("Build Time:", Date.now() - start);
+}
 
 // Execute Programs
-const programShell = spawn("./program.exe", shellArgs);
-const userShell = spawn("./user.exe", shellArgs);
+async function execute() {
+    let count = 0;
+    const start2 = Date.now();
+    const programShell = spawn(`./${name}_program.exe`, shellArgs);
+    const userShell = spawn(`./${name}_user.exe`, shellArgs);
 
-programShell.stdout.on("data", (data) => {
-    console.log(`programShell: ~${data.toString().trim()}~`); // uh.
-    userShell.stdin.write(data);
-    //   programShell.stdin.end();
-});
+    programShell.stdout.on("data", (data) => {
+        if (logOutput) {
+            console.log(`programShell:\n${data.toString().trim()}`); // uh.
+            // console.log(`programShell: "${data.toString().trim()}"`); // uh.
+        }
+        userShell.stdin.write(data);
+        //   programShell.stdin.end();
+    });
 
-programShell.on("close", (code) => {
-    // console.log(`programShell: child process close all stdio with code ${code}`);
-});
+    programShell.stderr.on("data", (data) => {
+        // console.log(`${data.toString().trim()}`);
+        console.log(`programShell-err:\n${data.toString().trim()}`);
+    });
 
-programShell.on("exit", (code) => {
-    console.log(`programShell: child process exited with code ${code}`);
-});
+    programShell.on("close", (code) => {
+        // console.log(`programShell: child process close all stdio with code ${code}`);
+    });
 
-userShell.stdout.on("data", (data) => {
-    console.log(`userShell: ~${data.toString().trim()}~`); // uh.
-    programShell.stdin.write(data);
-});
+    programShell.on("exit", (code) => {
+        if (logExit) {
+            console.log(`programShell: child process exited with code ${code}`);
+        }
+        if (++count == 2) {
+            console.log("Execute Time:", Date.now() - start2);
+            // execute();
+        }
+    });
 
-userShell.on("close", (code) => {
-    // console.log(`userShell: child process close all stdio with code ${code}`);
-});
+    userShell.stdout.on("data", (data) => {
+        if (logOutput) {
+            console.log(`userShell:\n${data.toString().trim()}`); // uh.
+            // console.log(`userShell: "${data.toString().trim()}"`); // uh.
+        }
+        programShell.stdin.write(data);
+    });
 
-userShell.on("exit", (code) => {
-    console.log(`userShell: child process exited with code ${code}`);
-});
+    userShell.stderr.on("data", (data) => {
+        console.log(`userShell:\n${data.toString().trim()}`);
+    });
+
+    userShell.on("close", (code) => {
+        // console.log(`userShell: child process close all stdio with code ${code}`);
+    });
+
+    userShell.on("exit", (code) => {
+        if (logExit) {
+            console.log(`userShell: child process exited with code ${code}`);
+        }
+        if (++count == 2) {
+            console.log("Execute Time:", Date.now() - start2);
+            // execute();
+        }
+    });
+}
 
 process.on("exit", (code) => {
-    console.log("Execute Time:", Date.now() - start2);
-    console.log("Elapsed Time:", Date.now() - start);
+    console.log("Elapsed Time:", Number(performance.now().toFixed(0)));
     console.log(`About to exit with code: ${code}`);
 });
